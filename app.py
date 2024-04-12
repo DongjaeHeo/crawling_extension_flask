b7acab987d071f5de275498a95ed776916c9fef1
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-12 15:23:02 +0900

    post

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route('/')
 def home():
     return 'server start'
+
+
+@app.route('/post', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        # Get JSON data from the request
+        data = request.get_json()
+        # Return the received data as a JSON response
+        return jsonify(data)  # Use jsonify to ensure proper JSON formatting
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 8090, debug = True)
