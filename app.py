58d65aa4e60ff855583d604744fc4af75f6cfe97
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-12 15:48:25 +0900

    post

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return 'server start'
-
+    return 'Server started'
 
 @app.route('/post', methods=['POST'])
 def handle_post():
-    if request.method == 'POST':
-        # Get JSON data from the request
-        data = request.get_json()
-        # Return the received data as a JSON response
-        return jsonify(data)  # Use jsonify to ensure proper JSON formatting
+    # Ensure there is data in the request
+    if not request.is_json:
+        return jsonify({"error": "Missing JSON in request"}), 400
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data received"}), 400
+
+    return jsonify(data)  # Return the JSON data received
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port = 8090, debug = True)
+    app.run('0.0.0.0', port=8090, debug=True)
