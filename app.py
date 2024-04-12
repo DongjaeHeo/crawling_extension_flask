3426818aae44cb6a56a597eab072ecbdf79cd6d4
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-12 17:30:14 +0900

    return df

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -9,14 +11,70 @@ def home():
 @app.route('/post', methods=['POST'])
 def handle_post():
     # Ensure there is data in the request
+
     if not request.is_json:
         return jsonify({"error": "Missing JSON in request"}), 400
 
     data = request.get_json()
     if not data:
         return jsonify({"error": "No data received"}), 400
+    html_doc = data["html"]
 
-    return jsonify(data)  # Return the JSON data received
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    class_elements = defaultdict(list)
+
+    # 모든 태그를 찾아서 클래스 이름을 그에 해당하는 요소 리스트에 추가
+    for tag in soup.find_all(True):
+        class_names = tag.get('class', [])
+        for class_name in class_names:
+            class_elements[class_name].append(tag.get_text(strip=True))
+
+    # 각 클래스 이름에 해당하는 리스트의 길이를 맞추기 위해 빈 문자열로 채움
+    max_length = max(len(elements) for elements in class_elements.values())
+    for elements in class_elements.values():
+        elements.extend([''] * (max_length - len(elements)))
+
+    # pandas DataFrame 생성
+    df = pd.DataFrame(class_elements)
+
+    # CSV 파일로 저장
+    # df.to_csv('class_elements.csv', index=False, encoding='utf-8-sig')
+    # print("CSV 파일이 성공적으로 생성되었습니다.")
+
+
+
+
+
+
+
+    return df  # Return the JSON data received
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8090, debug=True)
+
+
+
+# html_doc = ""
+
+# soup = BeautifulSoup(html_doc, 'html.parser')
+
+# class_elements = defaultdict(list)
+
+# # 모든 태그를 찾아서 클래스 이름을 그에 해당하는 요소 리스트에 추가
+# for tag in soup.find_all(True):
+#     class_names = tag.get('class', [])
+#     for class_name in class_names:
+#         class_elements[class_name].append(tag.get_text(strip=True))
+
+# # 각 클래스 이름에 해당하는 리스트의 길이를 맞추기 위해 빈 문자열로 채움
+# max_length = max(len(elements) for elements in class_elements.values())
+# for elements in class_elements.values():
+#     elements.extend([''] * (max_length - len(elements)))
+
+# # pandas DataFrame 생성
+# df = pd.DataFrame(class_elements)
+
+# # CSV 파일로 저장
+# df.to_csv('class_elements.csv', index=False, encoding='utf-8-sig')
+# print("CSV 파일이 성공적으로 생성되었습니다.")
