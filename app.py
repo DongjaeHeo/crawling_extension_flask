82407eb974efda3f894396f0205d44abfee031cc
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-12 17:53:28 +0900

    cors

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 from collections import defaultdict
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return 'Server started'
