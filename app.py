8dd2aede67726f6b811f04a880b0a999e804abdb
Dongjae Heo <lindsay.heo@gmail.com>
2024-04-16 15:10:09 +0900

    clean up

--- app.py
+++ app.py
@@ -60,13 +60,9 @@ def handle_post():
 
     # CSV 파일로 저장
 
-    # csv_data = df.to_csv(index = False, encoding = 'utf-8')
+
     df.to_csv(f'class_elements{start_time}.csv', index=False, encoding='utf-8-sig')
-    # csv_data = pd.read_csv('class_elements.csv', encoding='utf-8-sig')
-    # response = Response(csv_data)
-    # response.headers["Content-Disposition"] = 'attachment; filename="class_elements.csv"'
-    # response.headers['Content-Type'] = 'text/csv'
-    # return response
+
     filename = f'class_elements{start_time}.csv'
 
     # remove file after download // 다운로드 후 파일 삭제
@@ -85,37 +81,7 @@ def handle_post():
 
 
 
-    # print("CSV 파일이 성공적으로 생성되었습니다.")
-#    Convert DataFrame to JSON
-    # result_json = df.to_json(orient='records')
-    # print(result_json)
-    # return jsonify({"data": result_json})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8090, debug=True)
-
-
-
-# html_doc = ""
-
-# soup = BeautifulSoup(html_doc, 'html.parser')
-
-# class_elements = defaultdict(list)
-
-# # 모든 태그를 찾아서 클래스 이름을 그에 해당하는 요소 리스트에 추가
-# for tag in soup.find_all(True):
-#     class_names = tag.get('class', [])
-#     for class_name in class_names:
-#         class_elements[class_name].append(tag.get_text(strip=True))
-
-# # 각 클래스 이름에 해당하는 리스트의 길이를 맞추기 위해 빈 문자열로 채움
-# max_length = max(len(elements) for elements in class_elements.values())
-# for elements in class_elements.values():
-#     elements.extend([''] * (max_length - len(elements)))
-
-# # pandas DataFrame 생성
-# df = pd.DataFrame(class_elements)
-
-# # CSV 파일로 저장
-# df.to_csv('class_elements.csv', index=False, encoding='utf-8-sig')
-# print("CSV 파일이 성공적으로 생성되었습니다.")
